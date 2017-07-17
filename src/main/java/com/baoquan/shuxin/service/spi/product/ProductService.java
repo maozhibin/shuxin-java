6e6f24ad7da0d767dbe0ac9a60d33cdf3e8ae06c
maozhi <maozhi2764147>
2017-07-17 09:59:30 +0800

    添加了产品列表功能

--- src/main/java/com/baoquan/shuxin/service/spi/product/ProductService.java
+++ src/main/java/com/baoquan/shuxin/service/spi/product/ProductService.java
@@ -3,7 +3,6 @@ package com.baoquan.shuxin.service.spi.product;
 import java.util.Map;
 
 import com.baoquan.shuxin.bean.Page;
-import com.baoquan.shuxin.model.product.Product;
 
 public interface ProductService {
     Page<Map<String,Object>> findListProduct(Page<Map<String, Object>> page,String name);
