b6d5c8c9baa450413a6bdf7ee5ae16c3814267c0
maozhi <maozhi2764147>
2017-07-26 19:26:17 +0800

    添加了计算当天0点和24点的方法

--- src/main/java/com/baoquan/shuxin/util/common/DateUtil.java
+++ src/main/java/com/baoquan/shuxin/util/common/DateUtil.java
@@ -1,8 +1,13 @@
 package com.baoquan.shuxin.util.common;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
+
+import org.apache.commons.lang3.time.DateUtils;
 
 public class DateUtil {
 	/*
@@ -20,32 +25,58 @@ public class DateUtil {
 		}
 		return ts;
 	}
-
-	public static long dateToStamp(Long time) {
-		Long ts = null;
+	/*
+	 * 将时间戳转换为时间
+	 */
+	public static String stampToDate(String s) {
+		String res;
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date date;
+		long lt = new Long(s);
+		Date date = new Date(lt);
+		res = simpleDateFormat.format(date);
+		return res;
+	}
+	public static String stampToDateY(String s) {
+		String res;
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		long lt = new Long(s);
+		Date date = new Date(lt);
+		res = simpleDateFormat.format(date);
+		return res;
+	}
+	
+
+	/**
+	 * 今天零点零分零秒的毫秒数
+	 */
+	public static long zero(String s) {
+		DateFormat fmt =new SimpleDateFormat("yyyy-MM-dd");
+		Long time=null;
 		try {
-			date = simpleDateFormat.parse(String.valueOf(time));
-			ts = date.getTime();
+			Date date = fmt.parse(s);
+			Date today = DateUtils.truncate(date, Calendar.DATE);
+			time=today.getTime();
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		return ts;
+		return time;
 	}
-	  /* 
-     * 将时间戳转换为时间
-     */
-    public static String stampToDate(String s){
-        String res;
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        long lt = new Long(s);
-        Date date = new Date(lt);
-        res = simpleDateFormat.format(date);
-        return res;
-    }
 
-	public static void main(String[] args) {
-		System.out.println(dateToStamp("2017-07-13"));
+	/**
+	 * 今天23点59分59秒的毫秒数
+	 */
+	public static long twelve(String s) {
+		DateFormat fmt =new SimpleDateFormat("yyyy-MM-dd");
+		Long time=null;
+		try {
+			Date date = fmt.parse(s);
+			Date today = DateUtils.truncate(date, Calendar.DATE);
+			Date lastday = DateUtils.addDays(today, +1);
+			time=lastday.getTime()-1;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return time;
 	}
+
 }
