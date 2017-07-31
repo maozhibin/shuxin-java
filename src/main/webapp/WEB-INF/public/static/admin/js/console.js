219cc34be43395f7735996b455cc882d413a64d5
maozhi <maozhi2764147>
2017-07-31 17:55:02 +0800

    前端页面修改

--- src/main/webapp/WEB-INF/public/static/admin/js/console.js
+++ src/main/webapp/WEB-INF/public/static/admin/js/console.js
@@ -17,7 +17,7 @@ $(function() {
 
     // 图表数据
     // 获取的折线图数据
-    var getData;
+    var getData=[];
     $.ajax({
         "url": "/admin/overview/moneyProfile?types=0",
         "data":{"types": 0},
@@ -27,9 +27,14 @@ $(function() {
         "success": function (result) {
             for(var k in result){
                 var dt={name:k,data:result[k]};
-                getData.push(dt);
+                console.log(dt);
+                getData[0]['name']=k;
+                getData[0]['data']=result[k];
+                // getData.push(dt);
+                console.log(typeof getData);
+                console.log(getData);
             }
-            console.log(getData);
+            console.log(getData[0]);
         }
     });
     $("#lastDay").click(function () {
@@ -42,8 +47,10 @@ $(function() {
             "success": function (result) {
                 for(var k in result){
                     var dt={name:k,data:result[k]};
-                    getData.push(dt);
+                    console.log(dt);
+                    getData[getData.length]=dt;
                 }
+                console.log(getData);
             }
         });
     });
@@ -57,24 +64,24 @@ $(function() {
             "success": function (result) {
                 for(var k in result){
                     var dt={name:k,data:result[k]};
-                    getData.push(dt);
+                    getData[getData.length]=dt;
                 }
             }
         });
     });
-    getData = [{
-        name: '2017/06/21',
-        data: [5, 20, 36, 10, 10, 20, 32, 55, 77, 32, 45, 66, 77, 24, 52, 54, 52, 66, 32, 34, 56, 43, 11, 34]
-    },{
-        name: '2017/06/22',
-        data: [10, 20, 52, 66, 32, 34, 32, 55, 77, 32, 45, 5, 20, 36, 10, 66, 77, 24, 52, 54, 56, 43, 11, 34]
-    },{
-        name: '2017/06/25',
-        data: [1, 2, 5, 6, 3, 4, 2, 5, 77, 32, 45, 5, 20, 36, 10, 66, 77, 24, 52, 54, 56, 43, 11, 34]
-    },{
-        name: '2017/06/05',
-        data: [11, 12, 51, 61, 13, 41, 12, 15, 177, 32, 45, 5, 20, 36, 10, 66, 77, 24, 52, 54, 56, 43, 11, 34]
-    }];
+    // getData = [{
+    //     name: '2017/06/21',
+    //     data: [5, 20, 36, 10, 10, 20, 32, 55, 77, 32, 45, 66, 77, 24, 52, 54, 52, 66, 32, 34, 56, 43, 11, 34]
+    // },{
+    //     name: '2017/06/22',
+    //     data: [10, 20, 52, 66, 32, 34, 32, 55, 77, 32, 45, 5, 20, 36, 10, 66, 77, 24, 52, 54, 56, 43, 11, 34]
+    // },{
+    //     name: '2017/06/25',
+    //     data: [1, 2, 5, 6, 3, 4, 2, 5, 77, 32, 45, 5, 20, 36, 10, 66, 77, 24, 52, 54, 56, 43, 11, 34]
+    // },{
+    //     name: '2017/06/05',
+    //     data: [11, 12, 51, 61, 13, 41, 12, 15, 177, 32, 45, 5, 20, 36, 10, 66, 77, 24, 52, 54, 56, 43, 11, 34]
+    // }];
 
     // 获取的饼图数据
     var getPieData = [{
